bee580253e64b17bd14acf38d2b848c330692c28
Daniel Khashabi <danielk@allenai.org>
2019-08-27 08:45:55 -0700

    adding the latest code for inference

--- examples/roberta/glue_inference.py
+++ examples/roberta/glue_inference.py
@@ -12,18 +12,24 @@ label_fn = lambda label: roberta.task.label_dictionary.string(
     [label + roberta.task.target_dictionary.nspecial]
 )
 ncorrect, nsamples = 0, 0
-# roberta.cuda()
+roberta.cuda()
 roberta.eval()
 with open('/Users/danielk/ideaProjects/fairseq/examples/roberta/glue_data/MNLI/dev_matched.tsv') as fin:
     fin.readline()
-    for index, line in tqdm(enumerate(fin)):
-        tokens = line.strip().split('\t')
-        sent1, sent2, target = tokens[8], tokens[9], tokens[15]
-        tokens = roberta.encode(sent1, sent2)
-        prediction = roberta.predict('sentence_classification_head', tokens).argmax().item()
-        prediction_label = label_fn(prediction)
-        ncorrect += int(prediction_label == target)
-        nsamples += 1
-        if index > 200:
-            break
+    with open('glue_data/MNLI/dev_matched_predictions.tsv', 'w+') as fout:
+        for index, line in tqdm(enumerate(fin)):
+            tokens = line.strip().split('\t')
+            sent1, sent2, target = tokens[8], tokens[9], tokens[-1]
+            tokens = roberta.encode(sent1, sent2)
+            prediction = roberta.predict('sentence_classification_head', tokens).argmax().item()
+            prediction_label = label_fn(prediction)
+            line2 = line.replace("\n", '')
+            fout.write(f"{tokens[0]}\t{sent1}\t{sent2}\t{target}\t{prediction_label}\n")
+            ncorrect += int(prediction_label == target)
+            nsamples += 1
+            # if index > 200:
+            #     break
+            if index % 100 == 0 and nsamples > 0:
+                print('| Accuracy: ', float(ncorrect) / float(nsamples))
+
 print('| Accuracy: ', float(ncorrect)/float(nsamples))
